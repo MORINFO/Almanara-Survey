09aa12c1d1363e60bf9a5b0b817bd9f315f03f60
Nicolas <nicolas.7l@hotmail.com>
2020-07-16 11:42:26 -0300

    melhoria no login do relatorio

--- public/js/relatorio.js
+++ public/js/relatorio.js
@@ -1,7 +1,12 @@
 async function filtrar() {
   event.preventDefault()
-  let tabela = '<tr  class="text-center"><th>Filial</th><th>Nome Funcionario</th><th>Matrícula/CPF</th><th>Temperatura</th><th> Você está com algum sintoma de gripe ou resfriado (tosse, coriza, espirros, etc.) ?</th><th>Na última semana você apresentou febre (acima de 37,8°C) ou sintomas de gripe associados com dificuldade para respirar?</th><th>Na última semana, você esteve em contato com algum caso confirmado de COVID-19?</th><th>Você já foi diagnosticado com COVID-19?*</th><th>Data e Hora da Pesquisa</th></tr>'
-  document.getElementById('tblData').innerHTML = '<h2 class="text-center">Carregando...<h2>'
+  let tabela = `<tr  class="text-center"><th>Filial</th><th>Nome Funcionario</th>
+  <th>Matrícula/CPF</th><th>Temperatura</th>
+  <th> Você está com algum sintoma de gripe ou resfriado (tosse, coriza, espirros, etc.) ?</th>
+  <th>Na última semana você apresentou febre (acima de 37,8°C) ou sintomas de gripe associados com dificuldade para respirar?</th>
+  <th>Na última semana, você esteve em contato com algum caso confirmado de COVID-19?</th><th>Você já foi diagnosticado com COVID-19?*</th>
+  <th>Data e Hora da Pesquisa</th></tr>'
+  document.getElementById('tblData').innerHTML = '<h2 class="text-center">Carregando...<h2>`
   await axios.post('/api/relatorio',
     {
       "Matricula": document.getElementById('matricula').value,
@@ -10,14 +15,20 @@ async function filtrar() {
     })
     .then(function (response) {
       response.data.map(pesquisa =>
-        tabela += `<tr class="text-center"><td>${pesquisa.Filial}</td><td>${pesquisa.NomeFuncionario}</td><td>${pesquisa.Matricula}</td><td>${pesquisa.Temperatura}</td><td>${pesquisa.Sintomas}</td><td>${pesquisa.FebreGripe}</td><td>${pesquisa.ContatoParente}</td><td>${pesquisa.HistoricoCovid} ${pesquisa.DiaDiagnosticado ? pesquisa.DiaDiagnosticado : ''}</td><td>${pesquisa.Data}  ${pesquisa.Hora}</td></tr>`)
+        tabela += `<tr class="text-center"><td>${pesquisa.Filial}</td>
+        <td>${pesquisa.NomeFuncionario}</td><td>${pesquisa.Matricula}</td>
+        <td>${pesquisa.Temperatura}</td><td>${pesquisa.Sintomas}</td>
+        <td>${pesquisa.FebreGripe}</td><td>${pesquisa.ContatoParente}</td>
+        <td>${pesquisa.HistoricoCovid} ${pesquisa.DiaDiagnosticado ? pesquisa.DiaDiagnosticado : ''}</td>
+        <td>${pesquisa.Data}  ${pesquisa.Hora}</td></tr>`)
       document.getElementById('tblData').innerHTML = tabela
     })
     .catch(function (erro) {
       console.log(erro)
       alert('Problema ao filtrar os dados ! Informar a MOR.')
       document.getElementById('form').reset()
-      document.getElementById('tblData').innerHTML = '<tr  class="text-center"><th>Nome Funcionario</th><th>Temperatura</th><th>Data e Hora da Pesquisa</th></tr>'
+      document.getElementById('tblData').innerHTML = `<tr  class="text-center"><th>Nome Funcionario</th>
+      <th>Temperatura</th><th>Data e Hora da Pesquisa</th></tr>`
     })
 
 }
@@ -40,9 +51,11 @@ function verificaLogado() {
   if (sessionStorage.getItem('sessao')) {
     document.getElementById('login').classList.add("esconder");
     document.getElementById('conteudo').classList.remove("esconder");
-  }else{
+    document.getElementById('conteudo').style.display = 'block';
+  } else {
     document.getElementById('login').classList.remove("esconder");
     document.getElementById('conteudo').classList.add("esconder");
+    document.getElementById('conteudo').style.display = 'none';
   }
 }
 verificaLogado()
@@ -61,6 +74,7 @@ async function logar() {
       sessionStorage.setItem('sessao', response.data)
       document.getElementById('login').classList.add("esconder");
       document.getElementById('conteudo').classList.remove("esconder");
+      document.getElementById('conteudo').style.display = 'block';
     })
     .catch(function (erro) {
       console.log(erro)
